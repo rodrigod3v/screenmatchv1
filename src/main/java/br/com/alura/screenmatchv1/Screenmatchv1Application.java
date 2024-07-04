181e759d3d036cc04d2851973c7fa9872557ea37
RodrigoD3V <rodrigod3vop@gmail.com>
2024-07-04 15:59:50 -0300

    attdoprojeto

--- src/main/java/br/com/alura/screenmatchv1/Screenmatchv1Application.java
+++ src/main/java/br/com/alura/screenmatchv1/Screenmatchv1Application.java
@@ -1,8 +1,7 @@
 package br.com.alura.screenmatchv1;
 
-import br.com.alura.screenmatchv1.model.DadosSerie;
-import br.com.alura.screenmatchv1.service.ConsumoApi;
-import br.com.alura.screenmatchv1.service.ConverteDados;
+
+import br.com.alura.screenmatchv1.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,15 +15,7 @@ public class Screenmatchv1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = ConsumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=aeab770b");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
